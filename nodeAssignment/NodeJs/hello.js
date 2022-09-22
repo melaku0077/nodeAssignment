d1305870bbbb6ba93c39c01cfe02c5f5298dc56e
Melaku G.Denbel <44544124+melaku0077@users.noreply.github.com>
2022-09-22 02:03:32 -0400

    node assignment

---
+++ nodeAssignment/NodeJs/hello.js
@@ -0,0 +1,23 @@
+// Melaku G.Denbel
+//WAP Assignment
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+   });
+
+   readline.question('What is your name? ', name => {
+    console.log(`Welcome ${name}`);
+        readline.question('Your age?', age => {
+            if(age < 16){
+                console.log("You're not allowed to drive in Iowa");
+            } 
+            else{
+            console.log("You're allowed to get a drivers license in Iowa");
+            }
+            readline.close();
+        })
+
+    });
+
+
---
+++ nodeAssignment/NodeJs/use_car_module.js
@@ -0,0 +1,8 @@
+// Melaku G.Denbel
+// WAP Assignment
+
+const car = require('./my_car');
+car.drive();
+car.turn(23);
+car.break();
+
