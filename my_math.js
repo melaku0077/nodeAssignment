36fa9cc4f7aee4d3bbf5f26067e9d1a00f20709b
Melaku G.Denbel <44544124+melaku0077@users.noreply.github.com>
2022-09-22 02:15:15 -0400

    Add files via upload

---
+++ my_math.js
@@ -0,0 +1,8 @@
+module.exports.add = (x,y) => x + y;
+module.exports.subtract = (x,y) => x - y;
+module.exports.multiply = (x,y) => x * y;
+module.exports.divide = (x,y) => x / y;
+
+module.exports.pi = 3.14;
+
+// export {add,subtract,multiply,divide};
